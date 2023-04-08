4ecb3a14578ea89d02433f8818ace3cfe77c098f
Tom Biscuit <chris.ailey90@gmail.com>
2023-04-08 01:27:13 +0100

    adjusted styling and layout for cart component

--- src/cart/Cart.js
+++ src/cart/Cart.js
@@ -84,27 +84,32 @@ export default function Cart(props) {
     // Mapped display of cart items
     const mapCart = props.cart.map((item, key) => (
     
-          <Card key={key} style={{width: '30rem'}} >
-              <Col style={{width: '10rem'}}>
-              {/* card image using the product's image URL at index 0 of the productimageurls array */}
-              <Card.Img src={item.product.productImageUrls[0]} style={{width: '10rem'}} />
-              </Col>
-                <Col>
-               <Card.Body style={{width: '20rem'}}>
-                   <Card.Title> {item.product.productName} </Card.Title>
-                   <Card.Text> £{item.product.productPrice} </Card.Text>
-                   {/* <Card.Text> Quantity: {props.productQuantity}</Card.Text> */}
-                   <Card.Text>
-                       Quantity:  <input className='cartQuantity' type="number" placeholder={"#"} value={item.cartQuantity} min="0" onChange={(e) => handleInputChange(e, item)}></input>
-                       {/* <Card.Text>{item.cartQuantity}</Card.Text> */}
-                    </Card.Text>
-                    <Card.Text>Subtotal: £{item.product.productPrice * item.cartQuantity}</Card.Text>
-                   {/* <input type="number" placeholder={countOccurrences(props.cart, item)} min="0" onChange={(e) => handleInputChange(e)}></input> */}
-                   <Button onClick={(e) => {handleRemoveFromCart(item)}}> Remove from Cart</Button>
-                    <Card.Link href="#" onClick={(e) => {handleUpdateCart(item)}}> Update Cart </Card.Link>
-               </Card.Body>
-               </Col>
-          </Card>
+        <Card key={key}  >
+            <div className='cart-card-flex'>
+                <div>
+                    <Col style={{width: '10rem'}}>
+                    {/* card image using the product's image URL at index 0 of the productimageurls array */}
+                    <Card.Img src={item.product.productImageUrls[0]} style={{width: '10rem'}} />
+                    </Col>
+                </div>
+                <div>
+                    <Col>
+                        <Card.Body style={{width: '20rem'}}>
+                            <Card.Title className='title'> {item.product.productName} </Card.Title>
+                            <Card.Text> £{item.product.productPrice} </Card.Text>
+                            {/* <Card.Text> Quantity: {props.productQuantity}</Card.Text> */}
+                            <Card.Text>
+                                Quantity:  <input className='cartQuantity' type="number" placeholder={"#"} value={item.cartQuantity} min="0" onChange={(e) => handleInputChange(e, item)}></input>
+                                {/* <Card.Text>{item.cartQuantity}</Card.Text> */}
+                            </Card.Text>
+                            <Card.Text>Subtotal: £{item.product.productPrice * item.cartQuantity}</Card.Text>
+                            {/* <input type="number" placeholder={countOccurrences(props.cart, item)} min="0" onChange={(e) => handleInputChange(e)}></input> */}
+                            <Button onClick={(e) => {handleRemoveFromCart(item)}}> Remove from Cart</Button>
+                        </Card.Body>
+                    </Col>
+                </div>
+            </div>
+        </Card>
    
     ));
     
@@ -123,13 +128,11 @@ export default function Cart(props) {
     }
   
   return (
-    <>
+    <div className='cart-container'>
         <h1> In your cart: </h1>
             {cartItems}
             <h4> Total: £{props.totalPrice} </h4>
-            <Button variant="primary" onClick={() => makeCart(props.cart)}>Go to Checkout</Button>
-     
-        
-    </>
+            <Button variant="primary" onClick={() => makeCart(props.cart)}>Go to Checkout</Button>  
+    </div>
   )
 }
